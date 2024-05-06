b3ab6730b3b31abfa2a907435658360bd75a4810
graydoubt <graydoubt@graydoubt.com>
2024-05-06 16:56:08 -0400

    feat: externalize animation resources, add trigger state

--- main.gd
+++ main.gd
@@ -5,6 +5,7 @@ var triggered: bool = false
 
 @export var automatic: bool = true
 
+
 func _on_button_down():
 	triggered = true
 
@@ -12,5 +13,7 @@ func _on_button_down():
 func _on_button_up():
 	triggered = false
 
+
+## Called from the fire animation
 func _fire():
-	print("%s::pew pew! (%d)" % [self, Time.get_ticks_msec()])
+	print("%s::_fire() -> pew pew! (%d)" % [self, Time.get_ticks_msec()])
